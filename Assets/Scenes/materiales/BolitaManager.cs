1de6dce116a4be5523258772feddc7b3a671e63e
IsmaelAsimbaya <85204681+IsmaelAsimbaya@users.noreply.github.com>
2022-12-14 12:24:06 -0500

    Initial commit

---
+++ Assets/Scenes/materiales/BolitaManager.cs
@@ -0,0 +1,271 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BolitaManager : MonoBehaviour
+{
+    public GameObject comida;
+    public int cantidadComida = 100;
+    public AgenteBolita agente;
+    public int cantidadAgente = 20;
+    public Depredador depredador;
+    public int cantidadDepredador = 3;
+
+    public List<GameObject> alimentos = new List<GameObject>();
+    public List<AgenteBolita> agentes = new List<AgenteBolita>();
+    public List<Depredador> depredadores = new List<Depredador>();
+
+    private int conteo = 0;
+    private float timer = 0;
+
+    void Start()
+    {
+        //creamos la comida
+        for (int n = 0; n < cantidadComida; n++)
+        {
+            GameObject tempAlimentos = Instantiate(comida,
+            new Vector3(Random.Range(-50, 50), Random.Range(-50, 50),
+            Random.Range(-50, 50)), Quaternion.identity);
+
+            alimentos.Add(tempAlimentos);
+        }
+        //creamos las bolitas
+        for (int n = 0; n < cantidadAgente; n++)
+        {
+            AgenteBolita tempAgentes = Instantiate(agente,
+            new Vector3(Random.Range(-50, 50), Random.Range(-50, 50),
+            Random.Range(-50, 50)), Quaternion.identity);
+
+            tempAgentes.PonID(conteo);
+            conteo++;
+
+            //colocamnos los gnes
+            //tamano
+            tempAgentes.PonGen(0, Random.Range(1.5f, 3.5f));
+            //color
+            tempAgentes.PonGen(1, Random.Range(0.0f, 1.0f));
+            tempAgentes.PonGen(2, Random.Range(0.0f, 1.0f));
+            tempAgentes.PonGen(3, Random.Range(0.0f, 1.0f));
+            //velocidad
+            tempAgentes.PonGen(4, Random.Range(5, 15));
+            //rango de vision
+            tempAgentes.PonGen(5, Random.Range(20, 150));
+            //costo
+            tempAgentes.PonGen(6, Random.Range(0.5f, 2.0f));
+            agentes.Add(tempAgentes);
+        }
+
+        //creamos los depredadores
+        for (int n = 0; n < cantidadDepredador; n++)
+        {
+            Depredador tempDepred = Instantiate(depredador,
+            new Vector3(Random.Range(-50, 50), Random.Range(-50, 50),
+            Random.Range(-50, 50)), Quaternion.identity);
+
+            tempDepred.PonID(conteo);
+            conteo++;
+
+            //colocamnos los gnes
+            //tamano
+            tempDepred.PonGen(0, Random.Range(1.5f, 3.5f));
+            //color
+            tempDepred.PonGen(1, Random.Range(0.0f, 1.0f));
+            tempDepred.PonGen(2, Random.Range(0.0f, 1.0f));
+            tempDepred.PonGen(3, Random.Range(0.0f, 1.0f));
+            //velocidad
+            tempDepred.PonGen(4, Random.Range(5, 15));
+            //rango de vision
+            tempDepred.PonGen(5, Random.Range(20, 150));
+            //costo
+            tempDepred.PonGen(6, Random.Range(0.5f, 2.0f));
+            depredadores.Add(tempDepred);
+        }
+    }
+
+    public void AdicionaAlimento(Vector3 pPosicion)
+    {
+        if (alimentos.Count < 200)
+        {
+            GameObject temp = Instantiate(comida, pPosicion, Quaternion.identity);
+            alimentos.Add(temp);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(alimentos.Count + ", " + agentes.Count);
+        timer += Time.deltaTime;
+
+        if (timer >= 0.5)
+        {
+            timer = 0;
+            AdicionaAlimento(new Vector3(Random.Range(-50, 50), Random.Range(-50, 50), Random.Range(-50, 50)));
+        }
+    }
+    public void Cruce(AgenteBolita p1, AgenteBolita p2)
+    {
+        if (agentes.Count > 100) return;
+
+        int propRecombinacion = 70;
+        int propMutacion = 10;
+
+        if (Random.Range(1, 100) < propRecombinacion)
+        {
+            //seleccionamos el punto de corte
+            int c = Random.Range(0, 7);
+            int n = 0;
+            AgenteBolita temp = Instantiate(agente, Vector3.zero, Quaternion.identity);
+
+            temp.PonID(conteo);
+            conteo++;
+
+            //copiamos de padre1 a hijo
+            for (n = 0; n < c; n++)
+            {
+                temp.PonGen(n, p1.ObtenGen(n));
+            }
+            //copiamos de padre2 a hijo
+            for (n = c; n < 7; n++)
+            {
+                temp.PonGen(n, p2.ObtenGen(n));
+            }
+            //vemos si hay mutacion
+            if (Random.Range(0, 100) < propMutacion)
+            {
+                //seleccionamos el gen
+                int gen = Random.Range(0, 7);
+                if (gen == 0)
+                    temp.PonGen(0, Random.Range(1.5f, 3.5f));
+                else if (gen == 1)
+                    temp.PonGen(1, Random.Range(0.0f, 1.0f));
+                else if (gen == 2)
+                    temp.PonGen(2, Random.Range(0.0f, 1.0f));
+                else if (gen == 3)
+                    temp.PonGen(3, Random.Range(0.0f, 1.0f));
+                else if (gen == 4)
+                    temp.PonGen(4, Random.Range(5, 15));
+                else if (gen == 5)
+                    temp.PonGen(5, Random.Range(20, 150));
+                else if (gen == 6)
+                    temp.PonGen(6, Random.Range(0.5f, 2.0f));
+            }
+            temp.energia = 500;
+            p1.energia = 500;
+            p2.energia = 500;
+
+            agentes.Add(temp);
+        }
+        else
+        {
+            AgenteBolita temp = Instantiate(agente,
+            Vector3.zero,
+            Quaternion.identity);
+
+            temp.PonID(conteo);
+            conteo++;
+
+            //colocamos los genes
+            //tamano 
+            temp.PonGen(0, p1.ObtenGen(0));
+            //color
+            temp.PonGen(1, p1.ObtenGen(1));
+            temp.PonGen(2, p1.ObtenGen(2));
+            temp.PonGen(3, p1.ObtenGen(3));
+            //velocidad
+            temp.PonGen(4, p1.ObtenGen(4));
+            //rango de vision
+            temp.PonGen(5, p1.ObtenGen(5));
+            //costo
+            temp.PonGen(6, p1.ObtenGen(6));
+            temp.energia = 500;
+            p1.energia = 500;
+            p2.energia = 500;
+
+            agentes.Add(temp);
+        }
+    }
+
+    public void CruceDepred(Depredador p1, Depredador p2)
+    {
+        if (depredadores.Count > 100) return;
+
+        int propRecombinacion = 70;
+        int propMutacion = 10;
+
+        if (Random.Range(1, 100) < propRecombinacion)
+        {
+            //seleccionamos el punto de corte
+            int c = Random.Range(0, 7);
+            int n = 0;
+            Depredador temp = Instantiate(depredador, new Vector3(40,40,0), Quaternion.identity);
+
+            temp.PonID(conteo);
+            conteo++;
+
+            //copiamos de padre1 a hijo
+            for (n = 0; n < c; n++)
+            {
+                temp.PonGen(n, p1.ObtenGen(n));
+            }
+            //copiamos de padre2 a hijo
+            for (n = c; n < 7; n++)
+            {
+                temp.PonGen(n, p2.ObtenGen(n));
+            }
+            //vemos si hay mutacion
+            if (Random.Range(0, 100) < propMutacion)
+            {
+                //seleccionamos el gen
+                int gen = Random.Range(0, 7);
+                if (gen == 0)
+                    temp.PonGen(0, Random.Range(1.5f, 3.5f));
+                else if (gen == 1)
+                    temp.PonGen(1, Random.Range(0.0f, 1.0f));
+                else if (gen == 2)
+                    temp.PonGen(2, Random.Range(0.0f, 1.0f));
+                else if (gen == 3)
+                    temp.PonGen(3, Random.Range(0.0f, 1.0f));
+                else if (gen == 4)
+                    temp.PonGen(4, Random.Range(5, 15));
+                else if (gen == 5)
+                    temp.PonGen(5, Random.Range(20, 150));
+                else if (gen == 6)
+                    temp.PonGen(6, Random.Range(0.5f, 2.0f));
+            }
+            temp.energia = 500;
+            p1.energia = 500;
+            p2.energia = 500;
+
+            depredadores.Add(temp);
+        }
+        else
+        {
+            Depredador temp = Instantiate(depredador,
+            new Vector3(40, 40, 0),
+            Quaternion.identity);
+
+            temp.PonID(conteo);
+            conteo++;
+
+            //colocamos los genes
+            //tamano 
+            temp.PonGen(0, p1.ObtenGen(0));
+            //color
+            temp.PonGen(1, p1.ObtenGen(1));
+            temp.PonGen(2, p1.ObtenGen(2));
+            temp.PonGen(3, p1.ObtenGen(3));
+            //velocidad
+            temp.PonGen(4, p1.ObtenGen(4));
+            //rango de vision
+            temp.PonGen(5, p1.ObtenGen(5));
+            //costo
+            temp.PonGen(6, p1.ObtenGen(6));
+            temp.energia = 500;
+            p1.energia = 500;
+            p2.energia = 500;
+
+            depredadores.Add(temp);
+        }
+    }
+
+}
---
+++ Assets/Scenes/materiales/Depredador.cs
@@ -0,0 +1,193 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Depredador : MonoBehaviour
+{
+    public Vector3 posicion;
+    public float energiaPareja = 1200;
+    private float[] cromosoma = new float[7];
+    //identificacion de los agentes
+    private int id = 0;
+    private int estado = 0;
+    public float energia = 1500;
+    private Vector3 pareja;
+    private float tamano = 1.0f;
+    private float velocidad = 0;
+    private float rango = 0;
+    private float costo = 1.5f;
+
+    //cambia el color de la bolita
+    private Material material;
+    private MeshRenderer mr;
+
+    private BolitaManager manager;
+
+    private AgenteBolita alimentoAgenteSeleccionado;
+    private Vector3 posAlimento = new Vector3();
+
+    private Depredador parejaSeleccionada;
+    private Vector3 posPareja = new Vector3();
+
+    private void Awake()
+    {
+        mr = GetComponent<MeshRenderer>();
+        material = mr.material;
+        manager = FindObjectOfType<BolitaManager>();
+    }
+
+    void Update()
+    {
+        posicion = transform.position;
+        transform.Translate(Vector3.forward * velocidad * Time.deltaTime);
+        //actualizamos la energia
+        energia -= costo;
+        if (energia <= 0) //verificamos si muere
+        {
+            estado = 2;
+        }
+        if (estado == 0) //buscar alimento selecciona el alimento mas cercano para acercarse a el.
+        {
+            float distancia = float.MaxValue;
+            float d = 0;
+            if (manager.agentes.Count == 0)
+            {
+                posAlimento = new Vector3(UnityEngine.Random.Range(-50, 50),
+                    UnityEngine.Random.Range(-50, 50), UnityEngine.Random.Range(-50, 50));
+                alimentoAgenteSeleccionado = null;
+            }
+            else foreach (AgenteBolita agente in manager.agentes)
+                {
+                    d = Vector3.SqrMagnitude(transform.position - agente.transform.position);
+                    if (d < distancia)
+                    {
+                        distancia = d;
+                        posAlimento = agente.transform.position;
+                        alimentoAgenteSeleccionado = agente;
+                    }
+                }
+            transform.LookAt(posAlimento);
+            estado = 1;
+        }
+        if (estado == 1) //perseguir alimento
+        {
+            //si el alimento ya fue tomado
+            if (alimentoAgenteSeleccionado == null)
+            {
+                estado = 0;
+            }
+            //verificamos si llegamos al alimento
+            else if (Vector3.Magnitude(transform.position - posAlimento) < 0.5f)
+            {
+                energia += (alimentoAgenteSeleccionado.GetComponent<AgenteBolita>().energia)/2;
+                //manager.AdicionaAlimento(transform.position);
+                alimentoAgenteSeleccionado.energia = 0;
+                //manager.agentes.Remove(alimentoAgenteSeleccionado);
+                //Destroy(alimentoAgenteSeleccionado);
+                
+                estado = 0;
+
+                if (energia > energiaPareja)
+                {
+                    estado = 3;
+                }
+            }
+        }
+        if (estado == 2) //muerto
+        {
+            //manager.AdicionaAlimento(transform.position);
+            manager.depredadores.Remove(this);
+            Depredador.DestroyImmediate(this.gameObject);
+        }
+        if (estado == 3) //buscaPareja
+        {
+            float distancia = float.MaxValue;
+            float d = 0;
+            parejaSeleccionada = null;
+
+            foreach (Depredador a in manager.depredadores)
+            {
+                if (a.energia >= energiaPareja && a.id != id)
+                {
+                    d = Vector3.Magnitude(a.transform.position - transform.position);
+                    if (d < distancia && d < cromosoma[5])
+                    {
+                        distancia = d;
+                        parejaSeleccionada = a;
+                    }
+                }
+            }
+            //verificamos que encontro pareja
+            if (parejaSeleccionada == null)
+            {
+                estado = 0;
+            }
+            else
+            {
+                estado = 4;
+            }
+        }
+        if (estado == 4)
+        { //persigue pareja
+            if (parejaSeleccionada == null)
+                estado = 0;
+            else
+            {
+
+                posPareja = parejaSeleccionada.posicion;
+                transform.LookAt(posPareja);
+
+                Debug.DrawLine(transform.position, parejaSeleccionada.transform.position);
+                if (Vector3.Magnitude(posPareja - transform.position) <
+                (cromosoma[0] + parejaSeleccionada.ObtenGen(0) + 1.5))
+                {
+                    estado = 5;
+                }
+            }
+            if (energia <= 0) estado = 2;
+            else if (energia < 500) estado = 0;
+        }
+        if (estado == 5)
+        { //reproduccion
+            manager.CruceDepred(this, parejaSeleccionada);
+            estado = 0;
+        }
+    }
+    //coloca valor dentro del cromosoma.
+    public void PonGen(int pIndice, float pValor)
+    {
+        cromosoma[pIndice] = pValor;
+        ActualizaCromosoma();
+    }
+
+    public float ObtenGen(int pIndice)
+    {
+        return cromosoma[pIndice];
+    }
+
+    public void PonID(int pId)
+    {
+        id = pId;
+    }
+
+    private void ActualizaCromosoma()
+    {
+        //colocamos el tamano
+        tamano = cromosoma[0];
+        transform.localScale = new Vector3(tamano, tamano, tamano);
+
+        //colocamos el color
+        Color temp = new Color(cromosoma[1], cromosoma[2], cromosoma[3]);
+        material.color = temp;
+
+        //colocamos la velocidad
+        velocidad = cromosoma[4];
+
+        //colocamos el rando de vision
+        rango = cromosoma[5];
+
+        //colocamos el costo
+        costo = cromosoma[6];
+
+    }
+}
